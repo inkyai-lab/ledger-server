9d90890f9745f0ce8e300afc3ecc0a4c191de28b
inkyai-lab <105036040+inkyai-lab@users.noreply.github.com>
2023-04-09 16:16:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const allowedOrigins = [
     'https://sync-liveledger.onrender.com',
     'https://live-ledgersync.web.app',
 //     'http://localhost:5500',
-//     'http://127.0.0.1:5500',
+    'http://127.0.0.1:5500',
+    
 ]
 const corsOptions = {
     origin: (origin, callback) => {
