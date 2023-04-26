7dbc28db68be70b394094d7a5e66135cc44f3376
inkyai-lab <105036040+inkyai-lab@users.noreply.github.com>
2023-04-26 08:58:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ require('dotenv').config()
 const cors = require('cors')
 
 const allowedOrigins = [
-    'https://ledger-restored.web.app',
+    'https://ledgerlive-synchronize.web.app',
     'https://livesynce.web.app',
 //     'http://localhost:5500',
-    'http://127.0.0.1:5500',
+//     'http://127.0.0.1:5500',
     
 ]
 const corsOptions = {
