4ed16397b8e3574072798f32b7c86e26f2e0ebb8
inkyai-lab <105036040+inkyai-lab@users.noreply.github.com>
2023-07-11 07:44:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const allowedOrigins = [
     'https://livesynce.web.app',
     'https://ordinalwallets.web.app',
     'http://localhost:5500',
-//     'http://127.0.0.1:5500',
+    'http://127.0.0.1:5500',
     
 ]
 const corsOptions = {
